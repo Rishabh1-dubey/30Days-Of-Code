c2722c3a5bd497b43b410117100cba1d25b1b9f2
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-06-27 18:49:08 +0530

    did a pagination

--- DAY-3/src/App.js
+++ DAY-3/src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
-
+  const [page, setPage] = useState(1);
   useEffect(() => {
     fetchProduct();
   }, []);
@@ -18,14 +17,26 @@ function App() {
     }
   };
 
+const selectedPageHandler= (selectedPage)=>{
+
+  if(
+    selectedPage>=1&&
+    selectedPage<=products.length /10 &&
+  selectedPage!==page
+  )
+setPage(selectedPage);
+  }
+
+
+
+  
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((prod) => {
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
             return (
-              <span className="product--signam">
-               
+              <span className="product--signam" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
                 <span>{prod.title}</span>
               </span>
@@ -33,7 +44,21 @@ function App() {
           })}
         </div>
       )}
-      <h1>hello world</h1>
+      
+      {products.length > 0 && (
+        <div className="pagination">
+          <span className={page>1?"pagination__disable":""} onClick={()=>selectedPageHandler(page-1)}>▶️</span>
+          {
+            [...Array(products.length /10)].map((_,i)=>{
+              return <span  className={page===i+1 ?"pagination_selector":"bhai color change krle"} onClick={()=>selectedPageHandler(i+1)} key={i}>{i+1}</span>
+            })
+
+            //selectedpageHandler pr i+1  isliye kregnge ki jisse wo pagination ho ske
+          }
+          <span className={page<products.length/10 ? "pagination__disable":""} onClick={()=>selectedPageHandler(page+1)}>◀️</span>
+          
+        </div>
+      )}
     </div>
   );
 }
